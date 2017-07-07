1168bf02f763fe70e837cafc7ccd8ae2f19465a4
TJ <TJFadness@gmail.com>
2017-07-07 09:46:47 -0600

    Restructured for Tile Kit folder, created data type for Brush Sets

---
+++ Assets/Editor/Tile Kit/BrushsetEditor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(Brushset))]
+public class BrushsetEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        //http://answers.unity3d.com/questions/8633/how-do-i-programmatically-assign-a-gameobject-to-a.html
+    }
+}
---
+++ Assets/Tile Kit/Scripts/Brushset.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Brushset", menuName = "Tile Kit/Brushset", order = 1)]
+public class Brushset : ScriptableObject {
+    public List<GameObject> Brushes;
+}
